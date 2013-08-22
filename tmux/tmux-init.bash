314e481f830c66e959c7db7d69851b260be0acf1
Jake Johnson <jake@archive.org>
2013-08-22 00:57:26 -0700

    Removed duplicated code from if clauses.

--- tmux/tmux-init.bash
+++ tmux/tmux-init.bash
@@ -1,51 +1,28 @@
 #!/bin/bash
 
-if [ $(uname) == "Darwin" ]; then
-    SESSION=$USER
-    tmux has-session -t $SESSION
-    if [ $? -eq 0 ]; then
-        echo "Session '$SESSION' already exists. Attaching..."
-        sleep 1
-        tmux attach -t $SESSION
-        exit 0;
-    fi
-    
-    tmux new-session -d -s $SESSION
-    
-    tmux new-window -t $SESSION
-    tmux rename-window 'home'
-    tmux send-keys 'cd ~' 'C-m'
-    
-    #tmux new-window -t $SESSION -n data
-    #tmux rename-window 'DATA'
-    #tmux send-keys 'cd /1/data/' 'C-m'
-    
-    tmux select-window -t $SESSION:0
-    
+SESSION=$USER
+tmux has-session -t $SESSION
+
+if [ $? -eq 0 ]; then
+    echo "Session '$SESSION' already exists. Attaching..."
+    sleep 1
     tmux attach -t $SESSION
+    exit 0;
 fi
 
-if [ $(uname) == "Linux" ]; then
-    SESSION=$USER
-    tmux has-session -t $SESSION
-    if [ $? -eq 0 ]; then
-        echo "Session '$SESSION' already exists. Attaching..."
-        sleep 1
-        tmux attach -t $SESSION
-        exit 0;
-    fi
-    
-    tmux new-session -d -s $SESSION
-    
-    tmux new-window -t $SESSION
-    tmux rename-window 'home'
-    tmux send-keys 'cd ~' 'C-m'
-    
+tmux new-session -d -s $SESSION
+tmux new-window -t $SESSION
+tmux rename-window 'home'
+tmux send-keys 'cd ~' 'C-m'
+
+tmux select-window -t $SESSION:0
+tmux attach -t $SESSION
+
+if [ $(hostname) =! "archive.org" ]; then
     tmux new-window -t $SESSION -n data
     tmux rename-window 'DATA'
     tmux send-keys 'cd /1/data/' 'C-m'
-    
-    tmux select-window -t $SESSION:0
-    
-    tmux attach -t $SESSION
 fi
+
+tmux select-window -t $SESSION:0
+tmux attach -t $SESSION
